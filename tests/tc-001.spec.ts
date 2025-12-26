cd4ac736e4d876e84f780cc7aacd39e8e561fdbe
hp-14-ce2008tx\Alisa <alisa.srikachar@gmail.com>
2025-12-27 00:08:28 +0700

    feat(tc-001): record step to test follow testcase TC_001 on test case document

---
+++ tests/tc-001.spec.ts
@@ -0,0 +1,10 @@
+import { test, expect } from '@playwright/test';
+
+test('tc-001', async ({ page }) => {
+  await page.goto('https://abhigyank.github.io/To-Do-List/');
+  await page.locator('#new-task').click();
+  await page.locator('#new-task').fill('Test');
+  await page.getByRole('button', { name: 'add' }).click();
+  await page.getByRole('link', { name: 'To-Do Tasks' }).click();
+  await expect(page.locator('#todo.is-active #incomplete-tasks #text-1')).toHaveText('Test');
+});
